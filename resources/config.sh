35265bc860182f2b66a4daca9f261c08935ad8f5
cmoulliard <cmoulliard@redhat.com>
2023-11-09 20:44:31 +0100

    Use sudo, add module iptable-nat
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- resources/config.sh
+++ resources/config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-systemctl --user enable --now podman.socket
+sudo systemctl --user enable --now podman.socket
 sudo loginctl enable-linger 1000
+
+sudo modprobe iptable-nat
 
 socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/var/run/user/1000/podman/podman.sock &
