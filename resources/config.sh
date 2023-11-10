8a027a64edf3de4df64ea16706b6e19674befaf9
cmoulliard <cmoulliard@redhat.com>
2023-11-10 07:18:01 +0100

    Fix wrong sock file path
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- resources/config.sh
+++ resources/config.sh
@@ -5,4 +5,4 @@ sudo loginctl enable-linger 1000
 
 sudo modprobe iptable-nat
 
-socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/var/run/user/1000/podman/podman.sock &
+socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/user/1000/podman/podman.sock &
