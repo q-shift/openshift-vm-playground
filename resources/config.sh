9150ce7d8838a5828fe17673badbe38128f0c3a2
cmoulliard <cmoulliard@redhat.com>
2023-11-10 13:04:30 +0100

    Starting podman as rootfull till we will have a workaround and till we will know if we will use podman rootfull or rootless. #12
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- resources/config.sh
+++ resources/config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 sudo dnf -y install podman socat
-systemctl --user enable --now podman.socket
-loginctl enable-linger 1000
+#systemctl --user enable --now podman.socket
+#loginctl enable-linger 1000
+sudo systemctl start podman.socket # systemctl enable podman.socket
+sudo systemctl enable podman.socket # systemctl start podman.socket
 
 sudo modprobe iptable-nat
 
-socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/user/1000/podman/podman.sock &
+# socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/user/1000/podman/podman.sock &
+sudo socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/podman/podman.sock &
