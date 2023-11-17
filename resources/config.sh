625e282f4198661e613b4eab2eeb942bca596c77
Ioannis Canellos <iocanel@gmail.com>
2023-11-17 10:02:38 +0200

    fix: use systemd to start socat for exposing podman

--- resources/config.sh
+++ resources/config.sh
@@ -7,6 +7,19 @@ sudo systemctl start podman.socket # systemctl enable podman.socket
 sudo systemctl enable podman.socket # systemctl start podman.socket
 
 sudo modprobe iptable-nat
+sudo cat > /etc/systemd/system/podman-remote.service <<EOF
+[Unit]
+Description=Podman Remote
+Requires=podman.socket
+After=network.target podman.socket
 
-# socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/user/1000/podman/podman.sock &
-sudo socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/podman/podman.sock &
+[Service]
+Restart=always
+ExecStart=socat TCP-LISTEN:2376,reuseaddr,fork,bind=0.0.0.0 unix:/run/podman/podman.sock
+
+[Install]
+WantedBy=default.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable podman-remote.service
